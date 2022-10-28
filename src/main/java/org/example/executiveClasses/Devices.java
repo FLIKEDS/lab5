1575b39b3054673e75cdffa2c9c86ecd43203df4
DESKTOP-5F3073M\#3736 <sasca.zernov@gmail.com>
2022-10-28 13:31:09 +0300

    Commit

---
+++ src/main/java/org/example/executiveClasses/Devices.java
@@ -0,0 +1,59 @@
+package org.example.executiveClasses;
+
+public class Devices {
+    String name;
+    int capacity;
+    String color;
+    int weight;
+    boolean onOff;
+    Devices devices[] = new Devices[10];
+
+    public Devices(String name, int capacity, String color, int weight) {
+        this.name = name;
+        this.capacity = capacity;
+        this.color = color;
+        this.weight = weight;
+        this.onOff = false;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public void setOnOff(boolean onOff) {
+        this.onOff = onOff;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public boolean isOnOff() {
+        return onOff;
+    }
+
+}
---
+++ src/main/java/org/example/interfaces/MainMenu.java
@@ -0,0 +1,5 @@
+package org.example.interfaces;
+
+public interface MainMenu {
+    void execute();
+}
